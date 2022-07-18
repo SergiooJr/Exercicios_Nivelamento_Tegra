41d992dd9fd0982a96c8c1a5dea8f45b8b9becd0
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-18 13:24:32 -0300

    Terminar exercicio 16 e 18

---
+++ Exercicios/Exercicio19/script.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", function(e){
+    e.preventDefault();
+    var sal = parseFloat(document.getElementById("sal").value);
+    var aumento;
+    var salNov;
+    var porcent = 0;
+
+    if(sal<1000){
+        porcent = 20;
+        aumento = sal*porcent/100;
+        salNov = sal+aumento;
+        document.getElementById("resul").innerHTML = "Novo salário = R$" + salNov.toFixed(2) + "<br>Aumento: R$" + aumento.toFixed(2) + "<br>Porcentagem: 20%";
+    }
+    else if(sal<3000){
+        porcent = 15;
+        aumento = sal*porcent/100;
+        salNov = sal+aumento;
+        document.getElementById("resul").innerHTML = "Novo salário = R$" + salNov.toFixed(2) + "<br>Aumento: R$" + aumento.toFixed(2) + "<br>Porcentagem: " + porcent + "%";
+    }
+    else if(sal<8000){
+        porcent = 10;
+        aumento = sal*porcent/100;
+        salNov = sal+aumento;
+        document.getElementById("resul").innerHTML = "Novo salário = R$" + salNov.toFixed(2) + "<br>Aumento: R$" + aumento.toFixed(2) + "<br>Porcentagem: " + porcent + "%";
+    }
+    else{
+        porcent = 05;
+        aumento = sal*porcent/100;
+        salNov = sal+aumento;
+        document.getElementById("resul").innerHTML = "Novo salário = R$" + salNov.toFixed(2) + "<br>Aumento: R$" + aumento.toFixed(2) + "<br>Porcentagem: " + porcent + "%";
+    }
+});
