0032fdb3412a17c69e57b1834cd58827b1756c94
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-19 11:28:17 -0300

    Exercicio21 terminado

--- Exercicios/Exercicio21/script.js
+++ Exercicios/Exercicio21/script.js
@@ -1,14 +1,14 @@
-var resul = document.querySelector("div.resul")
-const calcular = document.querySelector("button")
+var resul = document.querySelector("div.resul");
+const calcular = document.querySelector("button");
 
 calcular.addEventListener("click", () => {
     var n = parseInt(document.getElementById("x").value);
-
+    resul.innerHTML = "";
     for(var i=1; i<=n;i++){
         if(i%2==0){
-            var resultado = document.createElement("p")
-            resultado.innerHTML = i
-            resul.appendChild(resultado)
+            var resultado = document.createElement("p");
+            resultado.innerHTML = i;
+            resul.appendChild(resultado);
         }
     }
 });
