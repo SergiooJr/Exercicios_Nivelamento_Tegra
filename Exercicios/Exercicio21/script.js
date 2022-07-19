3498fc139f9128ccfed5ff83f97896d1cda4facf
Sérgio Augusto de Almeida Lima Júnior <junior.serginho18@gmail.com>
2022-07-19 10:37:36 -0300

    deu certo o 21

--- Exercicios/Exercicio21/script.js
+++ Exercicios/Exercicio21/script.js
@@ -1,13 +1,14 @@
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", function(e){
-    e.preventDefault();
-    var n = parseInt(document.getElementById("x").value);
-    var nF = parseInt(document.getElementById("numF").value);
-    var total = 0;
-    var i = 0;
+var resul = document.querySelector("div.resul")
+const calcular = document.querySelector("button")
 
-    for(i = nI; i<=nF; i++){
-        total = total + i;
+calcular.addEventListener("click", () => {
+    var n = parseInt(document.getElementById("x").value);
+
+    for(var i=1; i<=n;i++){
+        if(i%2==0){
+            var resultado = document.createElement("p")
+            resultado.innerHTML = i
+            resul.appendChild(resultado)
+        }
     }
-    document.getElementById("resul").innerHTML = "Resultado: " + total;
 });
